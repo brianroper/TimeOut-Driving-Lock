a001bc5d9fa05c2a4e578db9ee78b6811703d4bf
Brian Roper <broper138@gmail.com>
2017-08-30 10:08:49 -0400

    added correct date data to widget

--- app/src/main/java/com/brianroper/putitdown/widgets/CounterProvider.java
+++ app/src/main/java/com/brianroper/putitdown/widgets/CounterProvider.java
@@ -2,10 +2,12 @@ package com.brianroper.putitdown.widgets;
 
 import android.appwidget.AppWidgetManager;
 import android.appwidget.AppWidgetProvider;
+import android.content.ComponentName;
 import android.content.Context;
 import android.widget.RemoteViews;
 
 import com.brianroper.putitdown.R;
+import com.brianroper.putitdown.utils.Utils;
 
 /**
  * Created by brianroper on 8/10/17.
@@ -18,5 +20,12 @@ public class CounterProvider  extends AppWidgetProvider{
         super.onUpdate(context, appWidgetManager, appWidgetIds);
 
         RemoteViews remoteViews = new RemoteViews(context.getPackageName(), R.layout.widget_counter);
+
+        remoteViews.setTextViewText(R.id.widget_dayofweek, Utils.returnDayOfWeek());
+        remoteViews.setTextViewText(R.id.widget_date, Utils.returnFullDate());
+
+        ComponentName counterWidget = new ComponentName(context, CounterProvider.class);
+        AppWidgetManager manager = AppWidgetManager.getInstance(context);
+        manager.updateAppWidget(counterWidget, remoteViews);
     }
 }
