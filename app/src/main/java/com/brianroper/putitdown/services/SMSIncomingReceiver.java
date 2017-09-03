72b88f01e70e277d60c55c569c85ffffa1425690
Brian Roper <broper138@gmail.com>
2017-09-03 17:16:05 -0400

    added sms permissions and created sms broadcast receiver

---
+++ app/src/main/java/com/brianroper/putitdown/services/SMSIncomingReceiver.java
@@ -0,0 +1,20 @@
+package com.brianroper.putitdown.services;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.telephony.PhoneStateListener;
+import android.telephony.TelephonyManager;
+
+/**
+ * Created by brianroper on 9/3/17.
+ */
+
+public class SMSIncomingReceiver extends BroadcastReceiver {
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+
+    }
+
+}
