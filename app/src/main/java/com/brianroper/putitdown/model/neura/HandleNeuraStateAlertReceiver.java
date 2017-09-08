3abc50797654600f6644581f800f592667b1b1f7
Brian Roper <broper138@gmail.com>
2017-09-07 22:33:19 -0400

    added new permissions
    feat: added some location permissions to manifest
    
    feat: moved authentication code to NeuraManager class
    
    feat: added NeuraStateAlertReciever class

---
+++ app/src/main/java/com/brianroper/putitdown/model/neura/HandleNeuraStateAlertReceiver.java
@@ -0,0 +1,31 @@
+package com.brianroper.putitdown.model.neura;
+
+import android.content.Context;
+import android.widget.Toast;
+
+import com.neura.android.statealert.SensorsManager;
+import com.neura.resources.sensors.SensorType;
+import com.neura.standalonesdk.util.NeuraStateAlertReceiver;
+
+/**
+ * Created by brianroper on 9/7/17.
+ */
+
+public class HandleNeuraStateAlertReceiver extends NeuraStateAlertReceiver {
+    @Override
+    public void onDetectedMissingPermission(Context context, String permission) {
+        Toast.makeText(context, "Neura detected mission permission : " + permission, Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    public void onDetectedMissingPermissionAfterUserPressedNeverAskAgain(Context context, String permission) {
+        Toast.makeText(context, "Neura detected mission permission BUT user already pressed 'Never ask again': "
+                + permission, Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    public void onSensorStateChanged(Context context, SensorType sensorType, boolean isEnabled) {
+        Toast.makeText(context, "Neura detected that " + SensorsManager.getInstance().getSensorName(sensorType) +
+                " sensor is " + (isEnabled ? "enabled" : "disabled"), Toast.LENGTH_SHORT).show();
+    }
+}
--- app/src/main/java/com/brianroper/putitdown/model/neura/NeuraManager.java
+++ app/src/main/java/com/brianroper/putitdown/model/neura/NeuraManager.java
@@ -1,10 +1,24 @@
 package com.brianroper.putitdown.model.neura;
 
 import android.content.Context;
+import android.os.Bundle;
+import android.util.Log;
 
 import com.brianroper.putitdown.R;
+import com.brianroper.putitdown.views.DashboardActivity;
+import com.google.firebase.iid.FirebaseInstanceId;
+import com.neura.resources.authentication.AnonymousAuthenticateCallBack;
+import com.neura.resources.authentication.AnonymousAuthenticateData;
+import com.neura.resources.authentication.AnonymousAuthenticationStateListener;
+import com.neura.resources.authentication.AuthenticationState;
+import com.neura.sdk.callbacks.SubscriptionRequestCallbacks;
+import com.neura.sdk.object.AnonymousAuthenticationRequest;
 import com.neura.standalonesdk.service.NeuraApiClient;
 import com.neura.standalonesdk.util.Builder;
+import com.neura.standalonesdk.util.SDKUtils;
+
+import java.util.Arrays;
+import java.util.List;
 
 /**
  * Created by brianroper on 9/6/17.
@@ -36,4 +50,48 @@ public class NeuraManager {
         mNeuraApiClient.setAppSecret(context.getResources().getString(R.string.app_secret));
         mNeuraApiClient.connect();
     }
+
+    public void authenticateWithNeura(){
+        final String pushToken = FirebaseInstanceId.getInstance().getToken();
+
+        AnonymousAuthenticationRequest request = new AnonymousAuthenticationRequest(pushToken);
+
+        mNeuraApiClient.authenticate(request, new AnonymousAuthenticateCallBack() {
+            @Override
+            public void onSuccess(AnonymousAuthenticateData authenticationData) {
+                Log.i(getClass().getSimpleName(), "Successfully requested authentication with neura. " +
+                        "NeuraUserId = " + authenticationData.getNeuraUserId());
+
+                List<String> moments = Arrays.asList("userStartedWalking",
+                        "userStartedDriving", "userFinishedDriving",
+                        "userIsAboutToGoToSleep", "userStartedRunning");
+
+                for (int i = 0; i < moments.size(); i++) {
+                    String momentIdentifier = authenticationData.getNeuraUserId() + "_" + moments.get(i);
+                    mNeuraApiClient.subscribeToEvent(moments.get(i), momentIdentifier, mSubscriptionRequest);
+                }
+            }
+
+            @Override
+            public void onFailure(int errorCode) {
+                Log.e(getClass().getSimpleName(), "Failed to authenticate with neura. "
+                        + "Reason : " + SDKUtils.errorCodeToString(errorCode));
+                Log.i("NeuraError: ", errorCode+"");
+            }
+        });
+    }
+
+    private com.neura.sdk.service.SubscriptionRequestCallbacks mSubscriptionRequest =
+            new com.neura.sdk.service.SubscriptionRequestCallbacks() {
+
+        @Override
+        public void onSuccess(String momentName, Bundle bundle, String s1) {
+            Log.i(getClass().getSimpleName(), "Successfully subscribed to event : " + momentName);
+        }
+
+        @Override
+        public void onFailure(String momentName, Bundle bundle, int i) {
+            Log.w(getClass().getSimpleName(), "Failed to subscribe to event : " + momentName);
+        }
+    };
 }
