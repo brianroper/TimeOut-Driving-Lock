b8ce00d57c8fe24fca67fe04c849aef4e269562c
Brian Roper <broper138@gmail.com>
2017-09-14 15:21:16 -0400

    added slides

--- app/src/main/java/com/brianroper/putitdown/views/IntroActivity.java
+++ app/src/main/java/com/brianroper/putitdown/views/IntroActivity.java
@@ -1,5 +1,6 @@
 package com.brianroper.putitdown.views;
 
+import android.Manifest;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
@@ -25,12 +26,51 @@ public class IntroActivity extends AppIntro2 {
                 R.drawable.redcar,
                 getResources().getColor(R.color.colorPrimary)));
 
+        addSlide(AppIntro2Fragment.newInstance("Before we start",
+                "TimeOut needs permission to " +
+                        "access some of your device sensors. " +
+                        "Without them the app won't function properly",
+                R.drawable.redcar,
+                getResources().getColor(R.color.colorAccent)));
+
+        addSlide(AppIntro2Fragment.newInstance("Location",
+                "TimeOut uses location to determine if your vehicle is moving.",
+                R.drawable.redcar,
+                getResources().getColor(R.color.slide3Color)));
+
+        //askForPermissions(new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, 3);
+
+
+        addSlide(AppIntro2Fragment.newInstance("App Overlay",
+                "TimeOut needs permission to draw over other apps. " +
+                        "This is a key part of our lockout technology",
+                R.drawable.redcar,
+                getResources().getColor(R.color.slide4Color)));
+
+        //askForPermissions(new String[]{Manifest.permission.SYSTEM_ALERT_WINDOW}, 4);
+
+        addSlide(AppIntro2Fragment.newInstance("One Left",
+                "TimeOut needs access to the do not disturb permission. " +
+                        "This helps us keep your eyes on the road and not your phone",
+                R.drawable.redcar,
+                getResources().getColor(R.color.slide5Color)));
+
+
+        //askForPermissions(new String[]{Manifest.permission.MODIFY_AUDIO_SETTINGS}, 5);
+
+        addSlide(AppIntro2Fragment.newInstance("Start Driving",
+                "Time works silently in the background to ensure your always driving safetly. " +
+                        "To get start, just start driving!",
+                R.drawable.redcar,
+                getResources().getColor(R.color.slide6Color)));
+
+        addSlide(AppIntro2Fragment.newInstance("One Last Thing",
+                "To stay up to date on your safe driving progress. Click on the trip log to view a detailed breakdown.",
+                R.drawable.redcar,
+                getResources().getColor(R.color.colorPrimaryDark)));
+
         showSkipButton(false);
-        setProgressButtonEnabled(false);
-
-        setVibrate(true);
-        setVibrateIntensity(30);
-
+        setProgressButtonEnabled(true);
     }
 
     @Override
@@ -40,6 +80,7 @@ public class IntroActivity extends AppIntro2 {
 
     @Override
     public void onDonePressed(Fragment currentFragment) {
+        finish();
         super.onDonePressed(currentFragment);
     }
 
