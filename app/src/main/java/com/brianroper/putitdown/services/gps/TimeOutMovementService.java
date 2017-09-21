ad01ea45f62a263388ce13517ebe47e0a54f782c
Brian Roper <broper138@gmail.com>
2017-09-21 15:29:01 -0400

    uncommented gps code

--- app/src/main/java/com/brianroper/putitdown/services/gps/TimeOutMovementService.java
+++ app/src/main/java/com/brianroper/putitdown/services/gps/TimeOutMovementService.java
@@ -145,12 +145,12 @@ public class TimeOutMovementService extends Service implements TimeOutGpsListene
      * speed activity until it is disabled
      */
     private void updateSpeed(TimeOutLocation location){
-        mCurrentSpeed = 100;
+        mCurrentSpeed = 0;
 
-//        if(location != null){
-//            location.setUseMetricUnits(this.useMetricUnits());
-//            mCurrentSpeed = location.getSpeed();
-//        }
+        if(location != null){
+            location.setUseMetricUnits(this.useMetricUnits());
+            mCurrentSpeed = location.getSpeed();
+        }
 
         if(!mIsPassengerMode){
             if(!mIsUnlocked){
