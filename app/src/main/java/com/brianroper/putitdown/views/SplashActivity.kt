cc47a5ba11e86cba3253d14ab893c2cc8bc5bfec
Brian Roper <broper138@gmail.com>
2017-10-25 01:43:13 -0400

    implemented splash screen written in kotlin

---
+++ app/src/main/java/com/brianroper/putitdown/views/SplashActivity.kt
@@ -0,0 +1,24 @@
+package com.brianroper.putitdown.views
+
+import android.content.Intent
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+
+import com.brianroper.putitdown.R
+
+class SplashActivity : AppCompatActivity() {
+
+    internal var SPLASH_DURATION = 1300
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+
+        Handler().postDelayed({
+            startActivity(Intent(this, DashboardActivity::class.java))
+            //overridePendingTransition(R.transition.fade_in, R.transition.fade_out)
+            finish()
+        }, SPLASH_DURATION.toLong())
+    }
+}
